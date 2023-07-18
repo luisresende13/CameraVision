3ea104d8233cbad027bfeb724675fb8098565ba5
luisresende13 <luisresende13@gmail.com>
2023-07-18 20:38:24 -0300

    old api version restored

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,6 @@
 from main import app
-# import os
 
 if __name__ == "__main__":
-    print('wsgi.py __main__ EXECUTING...')
+    print('MAIN EXECUTION START');
     app.run()
     # app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
